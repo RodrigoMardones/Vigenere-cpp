5a33fc7835044bd61061794c6f773b699bf66e46
Rodrigo Mardones <rodrigo425tron@gmail.com>
2018-05-05 20:44:17 -0300

    creado de encriptado y eval de largo

--- src/Vigenere.cpp
+++ src/Vigenere.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 
+
 Vigenere::Vigenere(){
     //ctor
 }
@@ -12,10 +13,41 @@ Vigenere::Vigenere(string a){
     pass = a;
 }
 
-Vigenere::~Vigenere(){
-    //dtor
-}
 void Vigenere::set_pass(string a){
     pass = a;
 }
 
+string Vigenere::largo(string palabra){
+
+    int val = palabra.size();
+    //valor a retornar
+    string ret = "";
+    for(int i= 0; ;i++){
+        if(val==0){
+            i = 0;
+        }
+        if(pass.size() == palabra.size()){
+            break;
+        }
+        ret+=pass[i];
+    }
+    return ret;
+}
+
+string Vigenere::encriptar(string palabra){
+    //clave alargada para evaluar
+    string ret = largo(palabra);
+    for(int i=0;i<palabra.size();i++){
+        int a = (palabra[i] + ret[i])%26;
+        a+='A';
+        enc+=a;
+    }
+    return enc;
+}
+
+string Vigenere::desencriptar(string palabra){
+
+}
+Vigenere::~Vigenere(){
+    //dtor
+}
