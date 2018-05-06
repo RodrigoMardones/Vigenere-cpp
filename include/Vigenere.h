136ded6db543be0d286b776bf0e86c072d04b560
Rodrigo Mardones <rodrigo425tron@gmail.com>
2018-05-05 21:12:17 -0300

    agregado de encriptado

--- include/Vigenere.h
+++ include/Vigenere.h
@@ -9,18 +9,22 @@ class Vigenere
 {
     public:
         Vigenere();
+        //recibe como parametro a para setearlo como pass
         Vigenere(string a);
         void set_pass(string a);
-        string encriptar(string a);
-        string desencriptar(string a);
+        //recibe como parametro b para encriptar
+        string encriptar(string palabra);
+        //recibe como parametro b para desencriptar
+        string desencriptar(string palabra);
+        //recalcula el largo para evaluar
+        string largo(string palabra);
+
         virtual ~Vigenere();
 
     protected:
-        //dato a encriptar
-        string data = "";
+
         //clave de encriptacion
         string pass = "";
-        //dato encriptado
         string enc = "";
     private:
 
--- src/Vigenere.cpp
+++ src/Vigenere.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 
+
 Vigenere::Vigenere(){
     //ctor
 }
@@ -12,10 +13,56 @@ Vigenere::Vigenere(string a){
     pass = a;
 }
 
-Vigenere::~Vigenere(){
-    //dtor
-}
 void Vigenere::set_pass(string a){
     pass = a;
 }
 
+string Vigenere::largo(string palabra){
+
+    int val = palabra.size();
+    //valor a retornar
+    string ret = "";
+    for(int i= 0; ;i++){
+        if(val==0){
+            i = 0;
+        }
+        if(pass.size() == palabra.size()){
+            break;
+        }
+        ret+=pass[i];
+    }
+    return ret;
+}
+
+string Vigenere::encriptar(string palabra){
+    //clave alargada para evaluar
+    string ret = largo(palabra);
+    //comparacion de caracater por caracter para el encriptado del dato
+    for(int i=0;i<palabra.size();i++){
+        //evaluado del dato
+        int a = (palabra[i] + ret[i])%26;
+        //conversion a acsii
+        a+='A';
+        //sumado a atributo encriptado
+        enc+=a;
+    }
+    return enc;
+}
+
+string Vigenere::desencriptar(string palabra){
+
+    string ret = largo(palabra);
+    string des = "";
+
+    for(int i=0;i<palabra.size();i++){
+        int a = (enc[i]-ret[i] + 26 )%26;
+        a+='A';
+        des+=a;
+    }
+    //limpia encriptado
+    enc = "";
+    return des;
+}
+Vigenere::~Vigenere(){
+    //dtor
+}
